e8aec098011464bf53ff5036485ac75036eaa117
L-Kallas <141240168+L-Kallas@users.noreply.github.com>
2023-08-03 12:30:43 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -21,11 +21,12 @@ el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element X', el_list)
 y_axis = st.selectbox('select element Y', el_list)
 
+st.bokeh_chart(p, use_container_width=True)
 p = figure(title='simple line example', x_axis_label='x', y_axis_label='y')
 
-st.bokeh_chart(p, use_container_width=True)
 p.circle(df[x_axis]/10000, df[y_axis]/10000)
 p.line([np.min(df[x_axis]/10000), np.max(df[y_axis]/10000)], [5, 5])
+p.rect(x = 24, y = 4)
 show(p)
 
 #st.multiselect('select location', file_name_list, file_name_list[0])
