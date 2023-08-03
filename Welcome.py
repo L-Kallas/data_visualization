5d0361cf2a39d361d507fe364606610987546853
L-Kallas <141240168+L-Kallas@users.noreply.github.com>
2023-08-03 09:45:41 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -9,11 +9,11 @@ for i in os.listdir():
 
 st.write('Hello World')
 
-df = pd.read_csv('Bastar Craton.csv')
+select_file = st.multiselect('select location', file_name_list)
+
+df = pd.read_csv(select_file)
 st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
 #y_axis = st.selectbox('select element', el_list)
-
-st.multiselect('select location', file_name_list)
