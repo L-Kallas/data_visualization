61dab244aaee26210243f630190e5e0766274569
L-Kallas <141240168+L-Kallas@users.noreply.github.com>
2023-08-03 12:19:16 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -21,11 +21,9 @@ el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element X', el_list)
 y_axis = st.selectbox('select element Y', el_list)
 
-p = figure(
-    title='simple line example',
-    x_axis_label='x',
-    y_axis_label='y')
+p = figure(title='simple line example', x_axis_label='x', y_axis_label='y')
 
+p.circle(df[x_axis]/10000, df[y_axis]/10000)
 p.line([np.min(df[x_axis]/10000), np.max(df[y_axis]/10000)], [5, 5])
 
 #st.multiselect('select location', file_name_list, file_name_list[0])
