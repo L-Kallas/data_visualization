58e97c4eb9057271889d842e3610464c76b8b53a
L-Kallas <141240168+L-Kallas@users.noreply.github.com>
2023-08-03 12:29:26 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -14,7 +14,7 @@ st.write('Hello World')
 
 select_file = st.selectbox('select location', file_name_list)
 
-df = pd.read_csv (select_file)
+df = pd.read_csv ('Data/Bastar Craton.csv')#(select_file)
 st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
@@ -24,8 +24,8 @@ y_axis = st.selectbox('select element Y', el_list)
 p = figure(title='simple line example', x_axis_label='x', y_axis_label='y')
 
 st.bokeh_chart(p, use_container_width=True)
-p.circle(df['Si']/10000, df['Mg']/10000)
-#p.line([np.min(df[x_axis]/10000), np.max(df[y_axis]/10000)], [5, 5])
+p.circle(df[x_axis]/10000, df[y_axis]/10000)
+p.line([np.min(df[x_axis]/10000), np.max(df[y_axis]/10000)], [5, 5])
 show(p)
 
 #st.multiselect('select location', file_name_list, file_name_list[0])
