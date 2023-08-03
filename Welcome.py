0878acddae9c3a723870e128a635b51da80a8e44
L-Kallas <141240168+L-Kallas@users.noreply.github.com>
2023-08-03 12:33:22 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -27,6 +27,5 @@ p.circle(df[x_axis]/10000, df[y_axis]/10000)
 p.line([np.min(df[x_axis]/10000), np.max(df[y_axis]/10000)], [5, 5])
 p.rect(x = 24, y = 4)
 st.bokeh_chart(p, use_container_width=True)
-show(p)
 
 #st.multiselect('select location', file_name_list, file_name_list[0])
