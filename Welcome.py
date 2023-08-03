46999a4f70d20746daa03e05c340e0ac20ac7689
L-Kallas <141240168+L-Kallas@users.noreply.github.com>
2023-08-03 12:47:27 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,16 +12,16 @@ for i in os.listdir():
 
 st.write('Hello World')
 
-select_file = st.selectbox('select location', file_name_list)
+select_file = st.selectbox('Select Location', file_name_list)
 
 df = pd.read_csv(select_file)
 st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
-x_axis = st.selectbox('select element X', el_list)
-y_axis = st.selectbox('select element Y', el_list)
+x_axis = st.selectbox('Select Element X', el_list)
+y_axis = st.selectbox('Select Element Y', el_list)
 
-p = figure(title='simple line example', x_axis_label='x', y_axis_label='y', x_range(np.min(df[x_axis]/10000), np.max(df[x_axis]/10000)), y_range(np.min(df[y_axis]/10000), np.max(df[y_axis]/10000))
+p = figure(title='simple line example', x_axis_label=str.x_axis, y_axis_label='y')#, x_range(np.min(df[x_axis]/10000), np.max(df[x_axis]/10000)), y_range(np.min(df[y_axis]/10000), np.max(df[y_axis]/10000))
 
 p.circle(df[x_axis]/10000, df[y_axis]/10000)
 p.line([np.min(df[x_axis]/10000), np.max(df[y_axis]/10000)], [5, 5])
